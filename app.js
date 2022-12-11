9d471337e918508c323cbf166c45e8570775014a
aaryannipane <nipaneeducation@gmail.com>
2022-12-11 17:22:57 +0530

    init

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+require("dotenv/config");
+ 
+const app = express();
+
+// important or error will occur in production
+var port = process.env.PORT || 8000;
+
+// import routes
+const postsRoute = require('./routes/posts');
+
+// (MIDDLEWARES)
+app.use(cors({
+    origin: ["https://atmaapi.netlify.app"]
+})); // for cross domain acess to out api 
+// to parse the post data into json
+app.use(bodyParser.json());
+
+// middleware
+app.use("/posts", postsRoute);
+
+// routes
+app.get('/', (req, res)=>{
+    res.send("This is home page");
+})
+
+
+
+// database connection
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, (err)=>{
+    if(err) throw err;
+    console.log("Connected to database")
+})
+
+
+app.listen(port, ()=>{
+    console.log(`server running at port ${port}`);
+})
